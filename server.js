8ad26f7f7725729be3ee9493dfeeb022a60002bb
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-08-01 18:56:23 -0700

    update not working

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var configDB = require('./config/database.js');
 var Course = require('./app/models/course.js');
 
@@ -17,44 +17,46 @@ mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function() {
+app.configure(function () {
 
-	// set up our express application
-	app.use(express.static(__dirname + '/views'));
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
-	app.engine('html', require('ejs').renderFile);
-	app.set('view engine', 'html');
+    // set up our express application
+    app.use(express.static(__dirname + '/views'));
+    app.use(express.logger('dev')); // log every request to the console
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser()); // get information from html forms
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
     //app.set('view engine', 'ejs'); // set up ejs for templating
-    
 
-	// required for passport
-	app.use(express.session({ secret: 'slipknot' })); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
+
+    // required for passport
+    app.use(express.session({
+        secret: 'slipknot'
+    })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
 
 });
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-	// POST
-app.post('/course', function(req, res, next) {
-    console.log(req.body); 
+// POST
+app.post('/course', function (req, res, next) {
+    console.log(req.body);
     thiscourse = new Course(req.body);
-    thiscourse.save(function (err){
-        if (err){
+    thiscourse.save(function (err) {
+        if (err) {
             res.send(err);
         }
     })
 });
 // GET
-app.get('/course', function(req, res, next) {
+app.get('/course', function (req, res, next) {
     console.log(req.body)
     Course.find(function (err, docs) {
-        if(err)
-        res.send(err)
+        if (err)
+            res.send(err)
         res.json(docs)
     })
 })
@@ -62,31 +64,29 @@ app.get('/course', function(req, res, next) {
 // DELETE
 app.delete('/course/:id', (req, res) => {
     console.log(req.params);
-    Course.findByIdAndRemove(req.params.id, function(err, docs){
-        if(err)
-        res.send(err)
+    Course.findByIdAndRemove(req.params.id, function (err, docs) {
+        if (err)
+            res.send(err)
     });
     console.log('Deleted', req.params.id);
-    res.json({Message: "Item Deleted"})
-    
+    res.json({
+        Message: "Item Deleted"
+    })
+
 });
 
 // PUT
-app.put('/course/:id',(req, res) => {
-    console.log(req.params);
-    Course.findByIdAndUpdate(req.params.id, function(err, docs){
-        if(err)
-            res.send(err)
+app.put('/course/:id', function(req, res){
+    console.log(req.params.id);
+    console.log(req.body);
+    
+    Course.findByIdAndUpdate(req.params.id,  {
+        date: req.body.date
     });
-    console.log('PUT', req.params.id);
-    Course.update({
-        id: req.params.id,
-        date: req.body.date,
-        course: req.body.course,
-        location: req.body.location
+    res.status(204).end();
     });
+    
     console.log('Got HERE');
-})
 
 // launch ======================================================================
 app.listen(port);
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,31 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = require('chai').assert;
-const {app} = require('../server');
 const should = chai.should();
 const expect = require("chai").expect;
 const request = require('request');
+const app = require('../server');
 
 chai.use(chaiHttp);
 
 describe('LOCAL HOST / LOGIN', function () {
-    it('should respond to localhost', function () {
-        request.get(app),
-            function (error, reponse, body) {
-                expect(res.status).to.equal(200);
-                done();
-            };
+    it('should respond to localhost', function (done) {
+        chai.request(app).get('/')
+        .end(function (err, res) {
+            res.should.have.status(200)
+            done();
+        })
+        //,
+          //  function (error, reponse, body) {
+            //    expect(res.status).to.equal(404);
+              //  done();
+           // };
     });
 });
 
 describe('COURSES / PROFILE', function () {
     it('checking courses / profile', function () {
-        request.get('http://localhost:8080/profile'),
+        chai.request(app),
             function (error, reponse, body) {
                 expect(res.status).to.equal(200);
                 done();
@@ -30,12 +35,10 @@ describe('COURSES / PROFILE', function () {
 
 describe('SIGNUP', function () {
     it('checking signup', function () {
-        request.get('http://localhost:8080/signup'),
+        chai.request(app),
             function (error, reponse, body) {
                 expect(res.status).to.equal(200);
                 done();
             };
     });
 });
-
-
