047d3046fde8fc319f0ff6477e0b500c640fed41
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-08-04 21:40:50 -0700

    CLEANED UP

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = require('chai').assert;
-const should = require ('chai').should();
+const should = require('chai').should();
 const expect = chai.expect;
 const request = require('request');
 const app = require('../server');
@@ -11,20 +11,17 @@ chai.use(chaiHttp);
 describe('LOCAL HOST / LOGIN', function () {
     it('should respond to localhost', function () {
         chai.request(app).get('/'),
-        function (err, res, body) {
-            expect(res.status).equal(200);
-            
-        }
-        });
+            function (err, res, body) {
+                expect(res.status).equal(200);
+            }
     });
-;
+});;
 
 describe('COURSES / PROFILE', function () {
     it('checking courses / profile', function () {
         chai.request(app).get('../profilesss'),
             function (error, response, body) {
                 expect(res.status).to.equal(200);
-                
             };
     });
 });
@@ -34,6 +31,6 @@ describe('SIGNUP', function () {
         chai.request(app),
             function (error, response, body) {
                 expect(res.status).to.equal(200);
-            
-    }});
+            }
+    });
 });
